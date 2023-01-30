7490da6b1b8103d59890dd5d5a78e09c992b6acf
Thomas <84379061+KomaedaN@users.noreply.github.com>
2023-01-30 19:19:48 +0100

    first point valid

--- oc_lettings_site/admin.py
+++ oc_lettings_site/admin.py
@@ -4,11 +4,6 @@ from .models import Letting
 from .models import Address
 from .models import Profile
 
-
-class AddressAdmin(admin.ModelAdmin):
-    verbose_name_plural = 'Address'
-
-
 admin.site.register(Letting)
-admin.site.register(Address, AddressAdmin)
+admin.site.register(Address)
 admin.site.register(Profile)
