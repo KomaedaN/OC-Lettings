88e506d6e408b78431b3413ce14f6e2f8752dcf3
Thomas <84379061+KomaedaN@users.noreply.github.com>
2023-02-08 15:35:25 +0100

    update config.yml

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,5 +1,5 @@
 import os
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -13,7 +13,7 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [config('HEROKU_HOST')]
 
 
 # Application definition
