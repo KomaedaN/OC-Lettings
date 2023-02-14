563f7ca319cf178a5d478efcfe48ee4dfcb05ffc
Thomas <84379061+KomaedaN@users.noreply.github.com>
2023-02-14 18:25:32 +0100

    update config.yml

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+def test_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path('', include('home.urls')),
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls),
+    path('sentry-debug', test_error),
 ]
